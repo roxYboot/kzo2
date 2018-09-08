f6b8ac1e0dc48565a7e01dd5157a668151afae31
roxYboot <43090878+roxYboot@users.noreply.github.com>
2018-09-08 20:51:30 +0300

    Update kzo2.js

--- kzo2.js
+++ kzo2.js
@@ -275,7 +275,7 @@ client.on('message', function(message) {
            msg.delete(5000);
           message.delete(5000);
         });
-    } else if(message.content.startsWith(prefix + "play")) {
+    } else if(message.content.startsWith(prefix + "=play")) {
 				        if(message.author.id !== myID) return;
             if(!args) return message.reply('اكتب الحالة اللي تريدها.');
         client.user.setGame(args);
